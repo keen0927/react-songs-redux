45cd32ef9540a23c905be690cb568452d5e59359
KEEN <kyh0927@live.co.kr>
2019-03-16 23:18:58 +0900

    19 03 16

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,4 @@
+// 액션 크리에이터
 export const selectSong = song => {
   // 리턴 액션
   return {
---
+++ src/reducers/index.js
@@ -0,0 +1,23 @@
+import { combineReducers } from 'redux';
+
+const songsReducer = () => {
+  return [
+    { title: 'No Scrubs', duration: '4:05' },
+    { title: 'macarena', duration: '2:30' },
+    { title: 'heal the wold', duration: '5:23' },
+    { title: 'The way you are', duration: '3:41' }
+  ]
+};
+
+const selectedSongReducer = (selectedSong = null, action) => {
+  if (action.type === 'SONG_SELECTED') {
+    return action.payload;
+  }
+
+  return selectedSong;
+};
+
+export default combineReducers({
+  songs: songsReducer,
+  selectedSong: selectedSongReducer
+});
